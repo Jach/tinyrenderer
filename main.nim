99a7062ef9e39534b4af2154de5f4d727ef74d5a
Kevin Secretan <kevinmccloud@gmail.com>
2016-08-09 02:26:08 -0700

    Upside down face render!

--- main.nim
+++ main.nim
@@ -1,5 +1,6 @@
-
+import parseopt2
 import tgaimage
+import model as m
 
 # nim c -t="-pg" -l="-pg" main.nim && ./main && gprof main
 # though -d:release is easier to parse.
@@ -8,6 +9,8 @@ let
   red: TGAColor = constructTGAColor(255, 0, 0, 255)
   blue = constructTGAColor(0, 0, 255, 255)
   green = constructTGAColor(0, 255, 0, 255)
+  width = 800
+  height = 800
 
 proc line(image: var TGAImage; x0,y0,x1,y1:int; color:TGAColor) =
   var steep = false
@@ -44,10 +47,26 @@ proc line(image: var TGAImage; x0,y0,x1,y1:int; color:TGAColor) =
     x += 1
 
 when isMainModule:
-  var image: TGAImage = constructTGAImage(100, 100, FORMAT.RGB)
+  var model_filename = "obj/african_head.obj"
+  for kind, key, val in getopt():
+    case kind
+    of cmdArgument:
+      model_filename = key
+    else: assert(false, "no other opts supported")
 
-  image.line(13, 100-20, 80, 100-40, green)
-  image.line(20, 100-13, 40, 100-80, white)
-  image.line(80, 100-40, 13, 100-20, blue) # same line as 1
-  discard image.flip_vertically()
+  var image = constructTGAImage(width, height, FORMAT.RGB)
+  var model = constructModel(model_filename)
+
+  for i in 0..model.nfaces()-1:
+    let face = model.face(i)
+    for j in 0..2:
+      let
+        v0 = model.vert(face[j])
+        v1 = model.vert(face[(j+1) mod 3])
+        x0 = (v0.x+1)*width.float/2.0
+        y0 = (v0.y+1)*height.float/2.0
+        x1 = (v1.x+1)*width.float/2.0
+        y1 = (v1.y+1)*height.float/2.0
+      image.line(x0.int, y0.int, x1.int, y1.int, white)
+
   discard image.write_tga_file("output.tga", false)
