4f7a70344de5feb9bb550b6f6c3c7ca62a918af2
Kevin Secretan <kevinmccloud@gmail.com>
2018-03-03 23:53:40 -0800

    Single line

--- home.lisp
+++ home.lisp
@@ -6,10 +6,16 @@
 (defconstant +red+ (make-color 255 0 0 255))
 (defconstant +asset-dir+ "/home/kevin/git_repos/tinyrenderer/")
 
+(defun line (x0 y0 x1 y1 image color)
+  (loop for i from 0 to 1 by 0.01
+       do 
+       (let ((x (floor (+ (* x0 (- 1 i)) (* x1 i))))
+             (y (floor (+ (* y0 (- 1 i)) (* y1 i)))))
+         (setf (ppm-image-get image x y) color))))
+
 (defun main ()
   (let ((image (make-ppm-image 100 100 :RGB)))
-    (setf (ppm-image-get image 52 41) +red+)
-    (setf (ppm-image-get image 0 0) +white+)
+    (line 1 1 90 40 image +white+)
     (flip-vertically image) ; so that origin is at the left bottom corner...
     (write-ppm-image image +asset-dir+ "output.ppm")))
 
