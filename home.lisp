a185040bd592e677acb4481367b54eff30962f35
Kevin Secretan <kevinmccloud@gmail.com>
2018-03-04 02:01:55 -0800

    Add logging for line output to compare...

--- home.lisp
+++ home.lisp
@@ -1,57 +1,63 @@
 (defpackage home
-  (:use :cl :ppmimage))
+  (:use :cl :ppmimage :model))
 (in-package home)
 
 (defconstant +white+ (make-color 255 255 255 255))
 (defconstant +red+ (make-color 255 0 0 255))
 (defconstant +asset-dir+ "/home/kevin/git_repos/tinyrenderer/")
 
+(defconstant +width+ 100)
+(defconstant +height+ 100)
+
+(defparameter *model* (format nil "~A~A" +asset-dir+ "obj/african_head.obj"))
+
 (defun line (x0 y0 x1 y1 image color)
   (let ((steep nil))
     (when (< (abs (- x0 x1))
-           (abs (- y0 y1))) ; if the line is steep, transpose
+             (abs (- y0 y1))) ; if the line is steep, transpose
         (setf steep t)
         (rotatef x0 y0)
         (rotatef x1 y1))
     (when (> x0 x1) ; make it left-to-right
         (rotatef x0 x1)
         (rotatef y0 y1))
-    (loop for x from x0 to x1
-          do 
-          (let* ((i (/ (- x x0)
-                       (- x1 x0)))
-                 (y (floor (+ (* y0 (- 1 i)) (* y1 i)))))
+
+    (let* ((dx (- x1 x0))
+           (dy (- y1 y0))
+           (derror2 (* 2 (abs dy)))
+           (error2 0)
+           (y y0))
+      (loop for x from x0 below x1
+            do 
             (if steep
                 (setf (ppm-image-get image y x) color)
-                (setf (ppm-image-get image x y) color))))))
+                (setf (ppm-image-get image x y) color))
+            (incf error2 derror2)
+            (when (> error2 dx)
+              (incf y (if (> y1 y0) 1 -1))
+              (decf error2 (* 2 dx)))))))
+
+(defun draw-model (model image)
+  (loop for i below (model-nfaces model)
+        do
+        (let ((face (model-face model i))
+              (w/2 (ash +width+ -1))
+              (h/2 (ash +height+ -1)))
+          (loop for j below 3
+                do
+                (let* ((v0 (model-vert model (elt face j)))
+                       (v1 (model-vert model (elt face (mod (1+ j) 3))))
+                       (x0 (floor (* (1+ (vec3-x v0)) w/2)))
+                       (y0 (floor (* (1+ (vec3-y v0)) h/2)))
+                       (x1 (floor (* (1+ (vec3-x v1)) w/2)))
+                       (y1 (floor (* (1+ (vec3-y v1)) h/2))))
+                  (line x0 y0 x1 y1 image +white+))))))
 
 (defun main ()
-  (let ((image (make-ppm-image 100 100 :RGB)))
-    (line 13 20 80 40 image +white+)
-    (line 20 13 40 80 image +red+)
-    (line 80 40 13 20 image +red+)
+  (let ((image (make-ppm-image +width+ +height+ :RGB)))
+    (draw-model (make-model *model*) image)
     (flip-vertically image) ; so that origin is at the left bottom corner...
     (write-ppm-image image +asset-dir+ "output.ppm")))
 
 (main)
 
-#|
-(time
-  (let ((image (make-ppm-image 100 100 :RGB)))
-    (loop for i upto 1000000 do
-          (line 13 20 80 40 image +white+)
-          (line 20 13 40 80 image +red+)
-          (line 80 40 13 20 image +red+))))
-
-Evaluation took:
-  126.117 seconds of real time
-  126.249065 seconds of total run time (125.896683 user, 0.352382 system)
-  [ Run times consist of 3.001 seconds GC time, and 123.249 seconds non-GC time. ]
-  100.10% CPU
-  336,071,662,843 processor cycles
-  38,016,118,672 bytes consed
-
-My CPU is Intel i7 9200 at 2.79 ghz.
-This is without enabling any SBCL optimizations / declaring types.
-I don't think it's too bad... so I won't continue optimizing at the moment.
-#|
